9a70c085743399e4842f25c82ed8ad54560dbedb
Elias Kotlyar <elias.kotlyar@mediawave.de>
2017-10-19 17:14:31 +0200

    Fixxes #35

---
+++ Model/App/Config/ReaderPool.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Wirecard\CheckoutPage\Model\App\Config;
+
+class ReaderPool
+{
+    /**
+     * List of readers
+     *
+     * @var array
+     */
+    protected $_readers = [];
+
+    /**
+     * @param \Magento\Framework\App\Config\Scope\ReaderInterface[] $readers
+     */
+    public function __construct(
+        array $readers
+    ) {
+        $this->_readers = $readers;
+    }
+
+    /**
+     * Retrieve reader by scope type
+     *
+     * @param string $scopeType
+     * @return mixed
+     */
+    public function getReader($scopeType)
+    {
+        return $this->_readers[$scopeType];
+    }
+}
