f41d2e097ca44ec71a0aaec96dd258c5d2c8753e
Dino Kljuco <dinoklj@maestralsolutions.com>
2021-05-04 16:53:50 +0200

    Problem with sessions and losing cart in Chrome

---
+++ Model/Plugin/FixSession.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Qenta\CheckoutPage\Model\Plugin;
+
+use Magento\Framework\HTTP\Header;
+use Magento\Framework\Stdlib\Cookie\PhpCookieManager;
+use Magento\Framework\Stdlib\Cookie\PublicCookieMetadata;
+
+
+class FixSession
+{
+    /**
+     * @var Header
+     */
+    protected $header;
+
+    public function __construct(Header $header)
+    {
+        $this->header = $header;
+    }
+
+    public function beforeSetPublicCookie(
+        PhpCookieManager $subject,
+        $name,
+        $value,
+        PublicCookieMetadata $metadata = null
+    ) {
+        if ($metadata && method_exists($metadata, 'getSameSite') && ($name == 'PHPSESSID')) {
+            if ($metadata->getSameSite() != 'None') {
+                $metadata->setSameSite('None');
+                $metadata->setSecure(true);
+            }
+        }
+        return [$name, $value, $metadata];
+    }
+}
